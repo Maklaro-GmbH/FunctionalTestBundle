d8aab1521df8ddb75e92f3b38bb5091d4afaf305
Dragos Protung <dragos@protung.de>
2024-04-19 18:57:02 +0200

    Support twig function mock without using the build in assert method.

--- src/Test/Twig/TemplateTestCase.php
+++ src/Test/Twig/TemplateTestCase.php
@@ -15,7 +15,21 @@ use Twig\TwigFunction;
 
 abstract class TemplateTestCase extends KernelTestCase
 {
-    private bool $disableTwigCache = false;
+    /**
+     * When mocking a twig function, if the twig was already compiled and cached, we can not overwrite the function.
+     * We need to temporarily disable cache so that the twig file is recompiled with the mock as a reference to the function.
+     *
+     * Just disabling cache by calling `$twig->setCache(false)` is not enough if the compiled version was already loaded.
+     * We need to change the extension set signature, and we do this by adding a new extension.
+     */
+    private bool $cacheBusterExtensionAdded = false;
+
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+
+        $this->cacheBusterExtensionAdded = false;
+    }
 
     protected function mockTwigFunction(string $functionName, callable $callable): void
     {
@@ -23,9 +37,15 @@ abstract class TemplateTestCase extends KernelTestCase
 
         $twig->addFunction(new TwigFunction($functionName, $callable));
 
-        // when mocking a twig function, if the twig was already compiled and cached, we can not overwrite the function.
-        // we need to temporarily disable cache so that the twig file is recompiled with the mock as a reference to the function.
-        $this->disableTwigCache = true;
+        if ($this->cacheBusterExtensionAdded) {
+            return;
+        }
+
+        $twig->addExtension(
+            new class extends AbstractExtension {
+            },
+        );
+        $this->cacheBusterExtensionAdded = true;
     }
 
     /**
@@ -34,15 +54,6 @@ abstract class TemplateTestCase extends KernelTestCase
     protected function assertTwigTemplateEqualsHtmlFile(string $actualTwigTemplate, array $actualTwigTemplateContext): void
     {
         $twig = $this->getTwigEnvironment();
-
-        // just disabling cache by calling `$twig->setCache(false)` is not enough if the compiled version was already loaded.
-        // we need to change the extension set signature and we do this by adding a new extension
-        if ($this->disableTwigCache) {
-            $twig->addExtension(
-                new class extends AbstractExtension {
-                },
-            );
-        }
 
         $actual = $twig->render($actualTwigTemplate, $actualTwigTemplateContext);
         $actual = Str\trim($actual);
@@ -64,7 +75,7 @@ abstract class TemplateTestCase extends KernelTestCase
         }
     }
 
-    private function getTwigEnvironment(): Environment
+    protected function getTwigEnvironment(): Environment
     {
         return $this->getContainerService(Environment::class, 'twig');
     }
